62485f0ce2ae4e3032ce4280f48a1666db63298b
Evgeni Golov <evgeni@golov.de>
2018-02-08 15:34:08 +0100

    fix license classifier

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: GPLv2',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
     ],
