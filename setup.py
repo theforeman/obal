0e5231d9ee0d108187c80b0e92bc4aca701b2e4d
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-08-15 13:08:36 +0200

    Release 0.0.6

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def find_package_data(package, data_dir):
 
 setup(
     name='obal',
-    version='0.0.5',
+    version='0.0.6',
     description='packaging wrapper using ansible',
     long_description=long_description,
     long_description_content_type='text/markdown',
