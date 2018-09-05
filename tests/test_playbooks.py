1daffbb1d431bee75aa30c4a4cf92b965ff018a2
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-09-05 12:30:20 +0200

    Add tests to verify all playbook help texts

--- tests/test_playbooks.py
+++ tests/test_playbooks.py
@@ -32,3 +32,20 @@ def test_filename_matches_directory(playbook):
     filename = os.path.splitext(os.path.basename(playbook.path))[0]
     dirname = os.path.basename(os.path.dirname(playbook.path))
     assert filename == dirname
+
+
+def test_help(playbook, capsys, help_dir):
+    with pytest.raises(SystemExit) as excinfo:
+        obal.main([playbook.name, '--help'])
+    assert excinfo.value.code == 0
+
+    captured = capsys.readouterr()
+
+    help_file = help_dir / '{}.txt'.format(playbook.name)
+
+    try:
+        assert help_file.read() == captured.out
+    except IOError:
+        help_file.write(captured.out)
+
+        raise pytest.skip('Written help text')
