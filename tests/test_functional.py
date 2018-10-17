e67af5b6da771388d77f40909dfb6b5e89e3535f
Eric D. Helms <ericdhelms@gmail.com>
2018-10-16 20:19:58 -0400

    Add ability to specify lint configuration file for a package

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -431,7 +431,7 @@ def test_obal_bump_release_hello_with_changelog():
 def test_obal_lint_hello():
     assert_obal_success(['lint', 'hello'])
 
-    assert_mockbin_log(["rpmlint {pwd}/packages/hello"])
+    assert_mockbin_log(["rpmlint --file {pwd}/packages/hello/.rpmlintrc {pwd}/packages/hello"])
 
 
 @obal_cli_test(repotype='upstream_with_epoch')
@@ -445,7 +445,7 @@ def test_obal_lint_hello_with_epoch():
 def test_obal_lint_bad_changelog():
     assert_obal_failure(['lint', 'hello'])
 
-    assert_mockbin_log(["rpmlint {pwd}/packages/hello"])
+    assert_mockbin_log(["rpmlint --file {pwd}/packages/hello/.rpmlintrc {pwd}/packages/hello"])
 
 
 @obal_cli_test(repotype='upstream')
