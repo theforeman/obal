e32de29efdff926ee1beca6c17460919de5bb262
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-03-15 00:09:37 +0100

    Implement more build methods

--- obal/data/modules/build_source.py
+++ obal/data/modules/build_source.py
@@ -3,8 +3,10 @@ Build a source package
 """
 
 import os
-from subprocess import CalledProcessError, check_output
+import re
 from contextlib import contextmanager
+from glob import glob
+from subprocess import CalledProcessError, check_output
 
 from ansible.module_utils.basic import AnsibleModule
 
@@ -39,17 +41,54 @@ def main():
     with chdir(directory):
         if os.path.exists(os.path.join(directory, 'Rakefile.dist')):
             try:
-                path = check_output(['rake', '-f', 'Rakefile.dist', 'pkg:generate_source'],
-                                    universal_newlines=True).rstrip()
+                output = check_output(['rake', '-f', 'Rakefile.dist', 'pkg:generate_source'],
+                                      universal_newlines=True).rstrip()
             except CalledProcessError as error:
                 module.fail_json(msg='Failed to build using Rakefile.dist',
                                  stdout=error.stdout, stderr=error.stderr)
+            if not output:
+                module.fail_json(msg='Failed to determine the output file')
+            path = output.splitlines()[-1]
+        elif glob(os.path.join(directory, '*.gemspec')):
+            gemspecs = glob(os.path.join(directory, '*.gemspec'))
+            if len(gemspecs) != 1:
+                gemspec = os.path.join(directory, os.path.basename(directory) + '.gemspec')
+                if gemspec not in gemspecs:
+                    module.fail_json(msg='Multiple gemspecs found', gemspecs=gemspecs)
+            else:
+                gemspec = gemspecs[0]
 
-            if not os.path.isabs(path):
-                path = os.path.join(directory, path)
+            try:
+                # TODO: use --output to set the path?
+                output = check_output(['gem', 'build', gemspec], universal_newlines=True)
+            except CalledProcessError as error:
+                module.fail_json(msg='Failed to build gemspec', gemspec=gemspec,
+                                 stdout=error.stdout, stderr=error.stderr)
+
+            match = re.search(r'File: (?P<path>.+\.gem)', output)
+            if not match:
+                module.fail_json(msg='Failed to determine gem file from output', output=output,
+                                 gemspec=gemspec)
+            path = match.group('path')
+        elif os.path.exists('Rakefile'):
+            # TODO: bundle install?
+            try:
+                output = check_output(['bundle', 'exec', 'rake', 'pkg:generate_source'],
+                                      universal_newlines=True).rstrip()
+            except CalledProcessError as error:
+                module.fail_json(msg='Failed to build using Rakefile.dist',
+                                 stdout=error.stdout, stderr=error.stderr)
+            if not output:
+                module.fail_json(msg='Failed to determine the output file')
+            path = output.splitlines()[-1]
         else:
             module.fail_json(msg='Unsupported package directory')
 
+    if not os.path.isabs(path):
+        path = os.path.join(directory, path)
+
+    if not os.path.exists(path):
+        module.fail_json(msg="Determined path doesn't exist", path=path)
 
     module.exit_json(changed=path is not None, path=path)
 
