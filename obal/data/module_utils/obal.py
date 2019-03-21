069cd5e06b035b93e77bd3d8b87b9f6dbf00ceb2
Adam Price <komidore64@gmail.com>
2019-03-21 14:56:42 -0400

    add package_whitelist_check
    The `package_whitelist_check` ansible module checks to see if the
    package it's given is properly whitelisted in the koji build system
    against the tags configured in rel-eng/releasers.conf. This is an
    attempt to catch packages that haven't been added _before_ being merged
    into the main development branch which causes build automation to fail.

--- obal/data/module_utils/obal.py
+++ obal/data/module_utils/obal.py
@@ -3,28 +3,61 @@ Ansible module helper functions for obal modules
 """
 import subprocess
 
-def get_changelog_evr(specfile):
-    """get the EVR from the last changelog entry in the specfile"""
-    evr = subprocess.check_output([
-        'rpm',
-        '--query',
-        '--changelog',
-        '--specfile',
-        specfile
-    ])
-    return evr.split("\n")[0].split(" ")[-1]
 
-def get_specfile_evr(specfile):
-    """get the EVR from the source header of the specfile"""
-    return subprocess.check_output([
+def specfile_macro_lookup(specfile, macro_str):
+    """expand a given macro from a specfile"""
+    cmd = [
         'rpmspec',
         '--query',
         '--queryformat',
-        '%{evr}',
+        macro_str,
         '--undefine',
         'dist',
         '--undefine',
         'foremandist',
         '--srpm',
         specfile
-    ])
+    ]
+    return subprocess.check_output(cmd)
+
+
+def get_changelog_evr(specfile):
+    """get the EVR from the last changelog entry in the specfile"""
+    cmd = [
+        'rpm',
+        '--query',
+        '--changelog',
+        '--specfile',
+        specfile
+    ]
+    evr = subprocess.check_output(cmd)
+    return evr.split("\n")[0].split(" ")[-1]
+
+
+def get_specfile_evr(specfile):
+    """get the EVR from the source header of the specfile"""
+    return specfile_macro_lookup(specfile, '%{evr}')
+
+
+def get_specfile_name(specfile):
+    """get the name from the specfile"""
+    return specfile_macro_lookup(specfile, '%{name}')
+
+
+def get_whitelist_status(build_command, tag, package):
+    """
+    Get whitelist status of a given package within a tag.
+
+    Return `True` if the package is whitelisted, `False` otherwise.
+    """
+    cmd = [
+        build_command,
+        'list-pkgs',
+        '--tag',
+        tag,
+        '--package',
+        package,
+        '--quiet'
+    ]
+    retcode = subprocess.call(cmd)
+    return retcode == 0
