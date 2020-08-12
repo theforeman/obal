779c297c7a6c6e932d0037666dcaa903b67e90d2
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-08-12 14:17:02 +0200

    Remove chdir in favor of the cwd param
    check_output can chdir itself, so there's no need to import the chdir
    context manager.

--- obal/data/module_utils/tito_wrapper.py
+++ obal/data/module_utils/tito_wrapper.py
@@ -3,12 +3,9 @@ A tito wrapper
 """
 from subprocess import STDOUT, check_output
 
-from ansible.module_utils.obal import chdir # pylint:disable=import-error,no-name-in-module
-
 
 def tito(command, directory):
     """
     Run a tito command
     """
-    with chdir(directory):
-        return check_output(command, stderr=STDOUT, universal_newlines=True)
+    return check_output(command, stderr=STDOUT, universal_newlines=True, cwd=directory)
