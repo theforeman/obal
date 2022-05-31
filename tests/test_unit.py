c5220cab782ad23407785835cec76b0e4bfd344e
Eric D. Helms <ericdhelms@gmail.com>
2022-05-31 10:46:56 -0400

    Use splitlines instead of split for getting evr from changelog

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,6 +1,10 @@
-from obal.data.module_utils.obal import get_specfile_sources
+from obal.data.module_utils.obal import get_specfile_sources, get_changelog_evr
 
 
 def test_get_specfile_sources():
     sources = get_specfile_sources('tests/fixtures/testrepo/upstream/packages/hello/hello.spec')
     assert sources == ['http://ftp.gnu.org/gnu/hello/hello-2.10.tar.gz']
+
+def test_get_changelog_evr():
+    evr = get_changelog_evr('tests/fixtures/testrepo/upstream/packages/hello/hello.spec')
+    assert evr == '2.10-2'
