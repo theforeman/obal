484e597dc317ee4169bf54bbe4eaa3b75eb70ab3
Eric D. Helms <ericdhelms@gmail.com>
2023-09-07 10:05:05 -0400

    Refactor repoclosure into a module

---
+++ obal/data/modules/repoclosure.py
@@ -0,0 +1,54 @@
+"""
+Run repoclosure on a set of repositories
+"""
+
+from subprocess import check_output, CalledProcessError, STDOUT
+from ansible.module_utils.basic import AnsibleModule
+
+
+def main():
+    """
+    Run repoclosure on a set of repositories
+    """
+    module = AnsibleModule(
+        argument_spec=dict(
+            config=dict(type='str', required=True),
+            check=dict(type='list', required=True),
+            additional_repos=dict(type='list', required=False, default=[]),
+            lookaside=dict(type='list', required=False, default=[]),
+        )
+    )
+
+    config = module.params['config']
+    check = module.params['check']
+    additional_repos = module.params['additional_repos']
+    lookaside = module.params['lookaside']
+
+    command = [
+        'dnf',
+        'repoclosure',
+        '--refresh',
+        '--newest',
+        '--config',
+        config
+    ]
+
+    for name in check:
+        command.extend(['--check', name])
+
+    for repo in additional_repos:
+        command.extend(['--repofrompath', f"{repo['name']},{repo['url']}"])
+
+    for repo in lookaside:
+        command.extend(['--repo', repo])
+
+    try:
+        output = check_output(command, universal_newlines=True, stderr=STDOUT)
+    except CalledProcessError as error:
+        module.fail_json(msg='Repoclosure failed', command=' '.join(command), output=error.output)
+
+    module.exit_json(changed=False, output=output)
+
+
+if __name__ == '__main__':
+    main()
